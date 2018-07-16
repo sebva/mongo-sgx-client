390a11191913bbecb2ed718e28c36c5baac64944
Sébastien Vaucher <seb.vaucher@gmail.com>
2018-07-16 15:13:39 +0200

    Initial commit

---
+++ trusted/mongoclient.cpp
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>      /* vsnprintf */
+
+#include "mongoclient.h"
+#include "mongoclient_t.h"  /* print_string */
+
+/* 
+ * printf: 
+ *   Invokes OCALL to display the enclave buffer to the terminal.
+ */
+void printf(const char *fmt, ...)
+{
+    char buf[BUFSIZ] = {'\0'};
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(buf, BUFSIZ, fmt, ap);
+    va_end(ap);
+    ocall_mongoclient_sample(buf);
+}
+
+int ecall_mongoclient_sample()
+{
+  printf("IN MONGOCLIENT\n");
+  return 0;
+}
+
---
+++ trusted/mongoclient.h
@@ -0,0 +1,17 @@
+#ifndef _MONGOCLIENT_H_
+#define _MONGOCLIENT_H_
+
+#include <stdlib.h>
+#include <assert.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+void printf(const char *fmt, ...);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* !_MONGOCLIENT_H_ */
