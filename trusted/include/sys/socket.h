3bc2f214762a5d6ffdd841604ad58eb8398a5ad4
Sébastien Vaucher <seb.vaucher@gmail.com>
2018-07-19 19:28:51 +0200

    Fix compilation on Ubuntu

--- trusted/include/sys/socket.h
+++ trusted/include/sys/socket.h
@@ -20,7 +20,30 @@
 #define	_SYS_SOCKET_H	1
 
 #include <sys/types.h>
-#include "/usr/include/bits/sockaddr.h"
+
+
+#ifndef _BITS_SOCKADDR_H
+#define _BITS_SOCKADDR_H	1
+
+
+/* POSIX.1g specifies this type name for the `sa_family' member.  */
+typedef unsigned short int sa_family_t;
+
+/* This macro is used to declare the initial common members
+   of the data types used for socket addresses, `struct sockaddr',
+   `struct sockaddr_in', `struct sockaddr_un', etc.  */
+
+#define	__SOCKADDR_COMMON(sa_prefix) \
+  sa_family_t sa_prefix##family
+
+#define __SOCKADDR_COMMON_SIZE	(sizeof (unsigned short int))
+
+/* Size of struct sockaddr_storage.  */
+#define _SS_SIZE 128
+
+#endif	/* bits/sockaddr.h */
+
+
 
 /* Structure describing a generic socket address.  */
 struct sockaddr {
